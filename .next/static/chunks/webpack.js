92e6a9b32c805fe1cab9e0843110d8314f71fb17
ybiill <billy.funciona@gmail.com>
2025-03-10 00:41:50 +0700

    fix-first version

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f2977e6b30e5df35"; }
+/******/ 		__webpack_require__.h = function() { return "e77bec1b56169475"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "*.{js,ts,jsx,tsx,mdx}",
     "app/**/*.{ts,tsx}",
